250beab17312dfca06625a4cb72482e6ee33a979
Matias Lanzi <mail@matiaslanzi.com>
2022-04-14 15:38:24 -0500

    Updated debugger settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "mlCommon.h"
 
 int main(int argc, char* argv[]){
-    std::cout << "\u001b[1J";
-    mlLog::Instance()->Info("Welcome to the program. I just started.\nWe'll begin by printing some messages to the log.\n");
-    mlLog::Instance()->Error("This is an error message.");
-    mlLog::Instance()->Warn("This is a warning");
-    mlLog::Instance()->Info("This is some debug trace");
+    
+    std::cout << "\nStarting\n";
 
-    std::cout << std::endl;
+    mlLog::Instance()->Info("This is a debug trace");
+    mlLog::Instance()->Warn("This is a warning");
+    mlLog::Instance()->Error("This is an error");
+    std::cout << "Quitting now!\n\n";
 
     return 0;
 }
